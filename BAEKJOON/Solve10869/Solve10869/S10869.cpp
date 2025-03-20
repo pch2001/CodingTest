17ca47463a29c623a70740b15ad0bbbd0296f0cf
pch2001 <pencil0303@nvaer.com>
2025-03-20 12:43:43 +0900

    March20th

---
+++ BAEKJOON/Solve10869/Solve10869/S10869.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;  
+
+int main() {     
+	long a, b;     
+	cin >> a >> b;     
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;	
+	cout << a % b << endl;
+
+
+
+
+}
