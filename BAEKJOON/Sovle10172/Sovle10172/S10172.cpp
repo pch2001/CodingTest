4636b4ea5dbc0ba84f49debaa9f133b36973dfaa
pch2001 <pencil0303@nvaer.com>
2025-03-27 12:29:51 +0900

    March27th

---
+++ BAEKJOON/Sovle10172/Sovle10172/S10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    cout << "|\\_/|" << std::endl;
+    cout << "|q p|   /}" << std::endl;
+    cout << "( 0 )\"\"\"\\" << std::endl;
+    cout << "|\"^\"`    |" << std::endl;
+    cout << "||_/=\\\\__|" << std::endl;
+
+}
