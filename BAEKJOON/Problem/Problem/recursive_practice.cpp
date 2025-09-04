274c012aced39d2366915e157b05eb85da6109d4
pch2001 <pencil0303@naver.com>
2025-09-04 23:42:12 +0900

    practice

--- BAEKJOON/Problem/Problem/recursive_practice.cpp
+++ BAEKJOON/Problem/Problem/recursive_practice.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-char path[5] = "";
+char path2[5] = "";
 void test2(int level) {
 	if (level == 3) {
 		//path[level] = '\0';   // 문자열 끝 표시
-		cout << path << endl;
+		cout << path2 << endl;
 		return;
 	}
 
 	for(size_t i = 0; i < 3; i++) {
-		path[level] = 'A' + i;
+		path2[level] = 'A' + i;
 		test2(level + 1);
-		path[level] = 0;
+		path2[level] = 0;
 	}
 
 }
