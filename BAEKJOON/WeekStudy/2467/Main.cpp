bee5dbe440de788056b7eb4939086663a1f379b1
박창후 <pencil0303@naver.com>
2026-04-09 14:08:20 +0900

    이분 탐색(#2467)
    - 투포인터로 풀었던 문제 이분 탐색으로 해결

--- BAEKJOON/WeekStudy/2467/Main.cpp
+++ BAEKJOON/WeekStudy/2467/Main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // Ãß°¡
+#include <algorithm>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	
+	int N=1;
+	std::cin >> N;
+
+	std::vector<int> solution(N);
+	
+	for (int i = 0; i < N; i++) 
+		std::cin >> solution[i];
+
+	long long minValue = 2000000002;
+	std::pair<int, int> answer;
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		int target = - solution[i];
+		auto it = std::lower_bound(solution.begin() + i + 1, solution.end(), target);
+		//std::cout << *it << " ";
+		if (it != solution.end())
+		{
+			long long sum = (long long)solution[i] + *it;
+			if (abs(sum) <= minValue)
+			{
+				minValue = abs(sum);
+				answer = {solution[i], *it};
+			}
+		}
+
+		if (it != solution.begin()+i+1)
+		{
+			auto prev = std::prev(it);
+			long long sum = (long long)solution[i] + *prev;
+			if (abs(sum) <= minValue)
+			{
+				minValue = abs(sum);
+				answer = { solution[i], *prev };
+			}
+		}
+	}
+	std::cout << answer.first << " " << answer.second;
+
+	return 0;
+}
+
+
+
+
+
 
 int main()
 {
