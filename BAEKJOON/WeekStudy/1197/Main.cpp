47017504ad32efc3997b51dc0479a5d5c7d19eba
박창후 <pencil0303@naver.com>
2026-04-13 19:54:54 +0900

    MST 문제 해결
    - 1197 해결

--- BAEKJOON/WeekStudy/1197/Main.cpp
+++ BAEKJOON/WeekStudy/1197/Main.cpp
@@ -1,29 +1,78 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int V, E;
-int parent[10001];
+//#define long long;
 
-int Find(int x)
+
+int find(std::vector<int>& parent, int start)
 {
-	if (parent[x] == x)
-		return x;
-	return parent[x] = Find(parent[x]);
+	if (parent[start] == start)
+		return start;
+	return parent[start] = find(parent, parent[start]);
 }
 
-int Union(int x, int y)
+void uni(std::vector<int>& parent,int start, int end)
+{			//이어주기
+	if (start != end)
+	{
+		parent[end] = start;
+	}
+
+}
+
+void StartMST(std::vector<int>& parent, const std::vector<std::pair<std::pair<int, int>, int>>& arr)
 {
-	x = Find(x);
-	y = Find(y);
-	if (x != y)
-		parent[x] = y;
+	long long totalWeight = 0;
+
+	int start = 0;
+	int end = 0;
+	int weight = 0;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		start = arr[i].first.first;
+		end = arr[i].first.second;
+		weight = arr[i].second;
+
+		start = find(parent, start);
+		end = find(parent, end);
+		if (start != end)
+		{
+			uni(parent, start, end);
+			totalWeight += weight;
+		} 
+	}
+	std::cout << totalWeight;
 }
 
 int main()
 {
+	int V, E;
+	int a, b, c;
 	std::cin >> V >> E;
-	for(int i = 0; i< V ; i++)
-		path[i] = i;
 
-	int i, j;
+	std::vector<std::pair<std::pair<int,int>, int>> arr;
+
+	for (int i = 0; i < E; i++)
+	{
+		std::cin >> a >> b >> c;
+		arr.emplace_back(std::make_pair(a,b),c);
+	}
+
+
+	std::sort(arr.begin(), arr.end(), [](auto& a, auto& b) {
+		return a.second < b.second;
+	});
+
+	std::vector<int> parent(V +1);
+
+	for (int i = 0; i < V + 1; i++)
+	{
+		parent[i] = i;
+	}
+
+	
+	StartMST(parent, arr);
+
 
 }
